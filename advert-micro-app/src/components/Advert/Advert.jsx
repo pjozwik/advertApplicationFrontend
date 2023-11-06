e43162c7f065d80843202fb0a258b25fe03346a3
Piotrek <piotrek.jozwik12@gmail.com>
2023-11-06 22:12:04 +0100

    Displaying and fetching adverts from a database

---
+++ advert-micro-app/src/components/Advert/Advert.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Advert.css'
+
+function Advert( {advert, imageSource, deleteAdvert} ) {
+
+  return (
+    <div className='advert-container' key={advert.id}>
+        <img className='advert-image' src={imageSource} alt=''></img>
+        <p className='advert-user'>pjoz</p>
+        <h1 className='advert-title'>{advert.title}</h1>
+        <span className='advert-price'>{advert.price}</span>
+        <button onClick={() => deleteAdvert(advert.id)} className='advert-btn'>
+          Delete
+        </button>
+    </div>
+  )
+}
+
+export default Advert
---
+++ advert-micro-app/src/components/AdvertForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdvertForm = () => {
+  return (
+    <div>AdvertForm</div>
+  )
+}
